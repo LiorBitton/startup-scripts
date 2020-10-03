0817fab9a87263c58fd10b39e41a3c09935bebb1
roil <liorb5000@gmail.com>
2020-10-03 23:06:19 +0300

    added debbuging mode

--- startup.py
+++ startup.py
@@ -1,10 +1,11 @@
 import os
 import win32gui, win32con
 import time
+print(os.environ["PYTHONHOME"])
 answer = input("Enter startup mode:")
-if answer == "":
+if answer == "" or "0":
     print("Custom startup initiating")
-    python_path =os.environ["PATH"]+ " "
+    python_path =os.environ["PYTHONHOME"]+ " "
     files = os.listdir('./')
     for file in files:
         if file.endswith('.py'):
@@ -13,8 +14,9 @@ if answer == "":
                 os.system(cmd)
                 print("excuted " + file)
                 time.sleep(0.5)
-    #the_program_to_hide = win32gui.GetForegroundWindow()
-    #win32gui.ShowWindow(the_program_to_hide , win32con.SW_HIDE)
+    if answer != "0":
+        the_program_to_hide = win32gui.GetForegroundWindow()
+        win32gui.ShowWindow(the_program_to_hide , win32con.SW_HIDE)
    
 else:
     print("Default startup initiated")
