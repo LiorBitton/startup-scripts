f731a8fd4ebdb851700d740c0b3e947e591caae9
roil <liorb5000@gmail.com>
2020-10-03 23:33:33 +0300

    should now found python path in any windows machine

--- startup.py
+++ startup.py
@@ -1,18 +1,28 @@
 import os
 import win32gui, win32con
 import time
-print(os.environ["PYTHONHOME"])
+import sys
+python_paths = []
+for path in os.environ["PATH"].split(";"):
+    if "Python" in path:
+        if "Scripts" not in path:
+            python_paths.append(path + 'pythonw.exe ')
+            print(path)
 answer = input("Enter startup mode:")
+
 if answer == "" or "0":
     print("Custom startup initiating")
-    python_path =os.environ["PYTHONHOME"]+ " "
     files = os.listdir('./')
     for file in files:
         if file.endswith('.py'):
             if file != os.path.relpath(__file__):
-                cmd = python_path + os.path.abspath(file)
-                os.system(cmd)
-                print("excuted " + file)
+                for path in python_paths:
+                    cmd = path + os.path.abspath(file)
+                    print("excuted " + file)
+                    try:
+                        os.system(cmd)
+                    except:
+                        print()
                 time.sleep(0.5)
     if answer != "0":
         the_program_to_hide = win32gui.GetForegroundWindow()
