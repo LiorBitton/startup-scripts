f5c2a9e6dd3f45e2a76931956e4191e701ce3cbc
roil <liorb5000@gmail.com>
2020-10-04 02:49:23 +0300

    trying to make the script run smoother

--- startup.py
+++ startup.py
@@ -7,22 +7,23 @@ for path in os.environ["PATH"].split(";"):
     if "Python" in path:
         if "Scripts" not in path:
             python_paths.append(path + 'pythonw.exe ')
-            
+files = os.listdir('./')
+scripts = []
+for file in files:
+    if file.endswith('.py'):
+        if file != os.path.relpath(__file__):
+            scripts.append(file)
 answer = input("Enter startup mode:")
-
 if answer == "" or "0":
-    print("Custom startup initiating")
-    files = os.listdir('./')
-    for file in files:
-        if file.endswith('.py'):
-            if file != os.path.relpath(__file__):
-                for path in python_paths:
-                    cmd = path + os.path.abspath(file)
-                    try:
-                        os.system(cmd)
-                        print("excuted " + file)
-                    except:
-                        print()
+    print("Initiating custom startup")
+    for file in scripts:
+        for path in python_paths:
+            cmd = path + os.path.abspath(file)
+            try:
+                os.system(cmd)
+                print("excuted " + file)
+            except:
+                print()
                 time.sleep(0.5)
     if answer != "0":
         the_program_to_hide = win32gui.GetForegroundWindow()
